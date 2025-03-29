bee308ba62ee7eec1493ff0c558fc513e240e5c4
Emil <stiiviboi@outlook.com>
2025-03-29 18:34:21 +0200

    Update 2sotoe_potcontrol_.ino
    added variables and fixed comment block

--- 2sotoe_potcontrol_/2sotoe_potcontrol_.ino
+++ 2sotoe_potcontrol_/2sotoe_potcontrol_.ino
@@ -4,20 +4,23 @@ This sketch allows you to sort of combine the power of two servos by placing the
 so you essentialy combine two servos power
 
 Connect potentiometer output to A0
-Connect servo signal wire to digital pin 9 (any pwm pin works just change the variable)
+Connect main servo signal wire to digital pin 9 and secondary servo signal wire to digital pin 10 (any pwm pin works just change the variable)
 Connect the rest of pot pins to 5V and ground and servo vcc and ground to arduino ground and 5V
 */
 #include <Servo.h>
 Servo servo1; // the servo on top
 Servo servo0;
 
+const int mainservo = 9; // assign digital pin 9 to var "mainservo"
+const int secondaryservo = 10; // assign digital pin 10 to var "secondaryservo"
 int const pot = A0;
 int potVal;
 int angle;
 
 void setup() {
-  servo0.attach(9); //attach to digital pin 9 on an arduino uno, mega 2560, nano or any pwm pin
-  servo1.attach(10); //attach to digital pin 10 on an arduino uno, mega 2560, nano or any pwm pin
+  //attach servos to correct pins
+  servo0.attach(mainservo);
+  servo1.attach(secondaryservo);
   Serial.begin(9600); 
 }
 
