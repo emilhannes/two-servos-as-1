fec342591864dc57a3f5208ea01018688ece2fbd
Emil <stiiviboi@outlook.com>
2025-06-06 23:53:51 +0300

    Update 2sotoe_potcontrol_.ino
    added comments, updated username

--- 2sotoe_potcontrol_/2sotoe_potcontrol_.ino
+++ 2sotoe_potcontrol_/2sotoe_potcontrol_.ino
@@ -1,5 +1,5 @@
 /*
-This sketch was made by Emil Hannes (ratew on github)
+This sketch was made by Emil Hannes (emilhannes on github)
 This sketch allows you to sort of combine the power of two servos by placing them on top and putting something inbetween
 so you essentialy combine two servos power
 
@@ -7,10 +7,13 @@ Connect potentiometer output to A0
 Connect main servo signal wire to digital pin 9 and secondary servo signal wire to digital pin 10 (any pwm pin works just change the variable)
 Connect the rest of pot pins to 5V and ground and servo vcc and ground to arduino ground and 5V
 */
+
+// include libraries and craete objects
 #include <Servo.h>
 Servo servo1; // the servo on top
 Servo servo0;
 
+// assign variables
 const int mainservo = 9; // assign digital pin 9 to var "mainservo"
 const int secondaryservo = 10; // assign digital pin 10 to var "secondaryservo"
 int const pot = A0;
@@ -21,21 +24,22 @@ void setup() {
   //attach servos to correct pins
   servo0.attach(mainservo);
   servo1.attach(secondaryservo);
-  Serial.begin(9600); 
+  Serial.begin(9600); //begin serial communication
 }
 
 void loop() {
+  // calculate the pot analog signal to a writeable servo pwm signal
   pot = analogRead(potPin);
   Serial.print("pot Value: ");
   Serial.print(pot);
   angle = map(pot, 0, 1023, 0, 179);
   Serial.print(", angle: ");
   Serial.println(angle);
-
+// the main math for 2sotoe
  servo0.write(angle);
  int othAngle = 180 -  angle;
  servo1.write(othAngle);
 
-delay(15);
+delay(15); // give the servo time to get to the correct position
 }
 
